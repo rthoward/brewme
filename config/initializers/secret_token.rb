d6ff5cb22ef5e52c648d2d65b113a77c6b79889c
a-out <richbone227@gmail.com>
2013-04-09 21:37:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewme::Application.config.secret_token = 'c0f37b7d9509ad00f42bc8d720c09baeeff39b79263d27a8065614b6b4594396d72b6fe522b8f53fb3852b16b345cf3ed0ed9f7b222a090ea3dfa767ea05ea1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brewme::Application.config.session_store :cookie_store, key: '_brewme_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brewme::Application.config.session_store :active_record_store
