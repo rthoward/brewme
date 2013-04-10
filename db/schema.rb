8a901e684b940e2c30f05513457d1465b0ddce59
a-out <richbone227@gmail.com>
2013-04-09 23:44:13 -0400

    ran migration for email index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130410030622) do
+ActiveRecord::Schema.define(:version => 20130410033118) do
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
@@ -30,5 +30,7 @@ ActiveRecord::Schema.define(:version => 20130410030622) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
