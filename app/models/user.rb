5d7cc50ca21c3d2e350ce914efdd5d5522b031e9
a-out <richbone227@gmail.com>
2013-04-09 23:26:03 -0400

    before_save hook for downcasing email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   validates :name,   presence: true, length: {maximum: 32}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  before_save { |user| user.email = email.downcase }
 end
