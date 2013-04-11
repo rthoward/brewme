af2a8b3e3f5a7c957f0e5ff06d25244e81a9b60b
a-out <richbone227@gmail.com>
2013-04-11 17:09:44 -0400

    ran migration for remember_token and index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130410034435) do
+ActiveRecord::Schema.define(:version => 20130411210236) do
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(:version => 20130410034435) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
