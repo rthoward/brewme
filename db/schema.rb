4ccca33f735ddb44b60bc6d78f1471d94bc15186
a-out <richbone227@gmail.com>
2013-04-11 18:27:04 -0400

    ran userid migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411210236) do
+ActiveRecord::Schema.define(:version => 20130411222625) do
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130411210236) do
     t.text     "instructions"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
