862adc828d68af166868ad2d2b1e7ee7fbfbd7da
a-out <richbone227@gmail.com>
2013-04-11 15:15:16 -0400

    create method for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
 end
