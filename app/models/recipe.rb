5b6be57d024ca23c016a5ecee399fa1542e58672
a-out <richbone227@gmail.com>
2013-04-11 18:19:01 -0400

    presence and length validations for instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe < ActiveRecord::Base
   attr_accessible :coffee, :coffee_amt, :instructions, :name, :water_amt, :water_temp
 
   validates :user_id, presence: true
+  validates :instructions, presence: true, length: { maximum: 10000 }
 
   default_scope order: 'recipes.created_at DESC'
 end
