4aaf643f0c6bf99a5d3b0e035e045a7ea366043d
a-out <richbone227@gmail.com>
2013-04-12 12:19:50 -0400

    attempt to redirect back, if signing page was redirected to

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # sign in and display profile (users#show)
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = "Invalid email / password combination"
       render 'new'
