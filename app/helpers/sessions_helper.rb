4a3f4892be71192e198a4e9e73a59ca6d55a7d3d
a-out <richbone227@gmail.com>
2013-04-12 11:59:42 -0400

    new helpers for verifying that correct user is logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,13 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -18,8 +25,26 @@ module SessionsHelper
     @current_user = user
   end
 
+  def current_user?(user)
+    user == current_user      
+  end
+
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end 
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 
 end
