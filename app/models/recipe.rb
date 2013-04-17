ea592d6346de65650e33a390800390e982600e88
a-out <richbone227@gmail.com>
2013-04-17 03:25:33 -0400

    added has_many relationship for mods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,13 @@
 class Recipe < ActiveRecord::Base
 
   belongs_to :user
+  has_many :mods, class_name: "Recipe", foreign_key: "original_recipe_id"
 
-  attr_accessible :coffee, :coffee_amt, :instructions, :name, :water_amt, :water_temp
+  attr_accessible :coffee, :coffee_amt, :instructions, :name, 
+  								:water_amt, :water_temp, :original_recipe_id
 
   validates :user_id, presence: true
   validates :instructions, presence: true, length: { maximum: 10000 }
 
   default_scope order: 'recipes.created_at DESC'
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411222625) do
+ActiveRecord::Schema.define(:version => 20130417070816) do
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(:version => 20130411222625) do
     t.integer  "water_amt"
     t.integer  "water_temp"
     t.text     "instructions"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
+    t.integer  "original_recipe_id"
   end
 
   create_table "users", :force => true do |t|
